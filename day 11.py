46e41d589a7f53935b418ddd61e081cfd116ec99
Jaishankar-2003 <jaishankar98421@gmail.com>
2024-03-29 15:10:30 +0530

    mouse operation practice

--- day 11.py
+++ day 11.py
@@ -19,18 +19,24 @@ driver = webdriver.Chrome()
 # 1 mouse hover - move_to_element(element)
 # 2 right click - context_click(element)
 # 3 double click - double_click(element)
+# 4 drag and drop - drag_and_drop(source,target)
+# 5 range slider - drag and drop by offset(element,x,y)
+# 6 scrolling page
 
-#Date picker
+# Date picker
 # ---------------
 # 1 standard
 # 2 non-standard(customized)
-#url = "https://jqueryui.com/datepicker/"
-#url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-#url = "https://api.jquery.com/contextmenu/"
+# url = "https://jqueryui.com/datepicker/"
+# url = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+# url = "https://api.jquery.com/contextmenu/"
 url = "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3"
+url ="http://www.dhtmlgoodies.com/scripts/drag-drop-custom/demo-drag-drop-3.html"
+url = "https://jqueryui.com/resources/demos/slider/range.html"
+url = "https://www.worldometers.info/geography/flags-of-the-world/"
 driver.get(url)
 driver.maximize_window()
-#--------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------
 
 # username_field = driver.find_element(By.CSS_SELECTOR, "input[placeholder='Username']")
 # username_field.send_keys("Admin")
@@ -55,7 +61,7 @@ driver.maximize_window()
 # # act.move_to_element(admin).move_to_element(use).click().perform()
 #
 # time.sleep(4)
-#-------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------
 
 # # right click
 # driver.implicitly_wait(10)
@@ -67,19 +73,72 @@ driver.maximize_window()
 # act.context_click(actt).perform()
 #
 # time.sleep(4)
-#------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------
 
-#double click
+# double click
 
-inner = driver.find_element(By.XPATH,"//iframe[@id='iframeResult']")
-driver.switch_to.frame(inner)
-field1 = driver.find_element(By.XPATH,"//input[@id='field1']")
-field1.click()
-field1.send_keys("welcome")
+# inner = driver.find_element(By.XPATH,"//iframe[@id='iframeResult']")
+# driver.switch_to.frame(inner)
+# field1 = driver.find_element(By.XPATH,"//input[@id='field1']")
+# field1.click()
+# field1.send_keys("welcome")
+#
+# button = driver.find_element(By.XPATH,"//button[normalize-space()='Copy Text']")
+# act = ActionChains(driver)
+# act.double_click(button).perform()
 
-button = driver.find_element(By.XPATH,"//button[normalize-space()='Copy Text']")
-act = ActionChains(driver)
-act.double_click(button).perform()
+# ----------------------------------------------------------------------------------------------------------------------
 
-#----------------------------------------------------------------------------------------------------------------------
+# drag and drop
+
+# driver.implicitly_wait(10)
+# source = driver.find_element(By.ID,"box6")
+# target = driver.find_element(By.ID,"box106")
+# act = ActionChains(driver)
+# act.drag_and_drop(source, target).perform()   #perform drag and drop perform
+
+# -----------------------------------------------------------------------------------------------------------------------
+
+# range slider
+
+# min = driver.find_element(By.XPATH,"//*[@id='slider-range']/span[1]")
+# max = driver.find_element(By.XPATH,"//*[@id='slider-range']/span[2]")
+#
+# print("location of before move",min.location) #{'x': 202, 'y': 46}
+# print("location of max slider",max.location) #{'x': 808, 'y': 46}
+#
+# act = ActionChains(driver)
+# act.drag_and_drop_by_offset(min,100,0).perform()
+# act.drag_and_drop_by_offset(max,-100,0).perform()
+#
+# print("location of after move",min.location)
+# print("location of max slider",max.location)
+
+# --------------------------------------------------------------------------------------------------------------------
+
+# scroller by using javascript scripts
+
+# 1. scroll down by pixel
+driver.execute_script("window.scrollBy(0,3000)","")
+value = driver.execute_script("return window.pageYOffset;")
+print("the num of value moved :",value) # 3000
+
+# scroll till the element is visible
+flag = driver.find_element(By.XPATH,"//img[@src='/img/flags/small/tn_in-flag.gif']")
+driver.execute_script("arguments[0].scrollIntoView();",flag)
+value = driver.execute_script("return window.pageYOffset;")
+print("The num of value moved :",value) # 4588
+
+# scroll till end of the page
+driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+value = driver.execute_script("return window.pageYOffset;")
+print("The num of value moved to end of page :",value) # 11393
+
+# scroll to initial position
+driver.execute_script("window.scrollBy(0,-document.body.scrollHeight)")
+value = driver.execute_script("return window.pageYOffset;")
+print("The num of value return to initial value :",value)
+
+# ---------------------------------------------------------------------------------------------------------
+
 
